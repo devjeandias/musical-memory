fdd841c2511cc0be2bd1a1add9ff9e6d69e490ad
Jean Carlos <jean.dias@ed3.com.br>
2024-03-19 23:22:19 -0300

    commit inicial

---
+++ src/GlobalStyle.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    *, *:before, *:after {
+        padding: 0;
+        margin: 0;
+    }
+
+    input, select, textarea {
+        apparence: none;
+    }
+`
